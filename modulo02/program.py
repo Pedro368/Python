9272fa365368ae506eb54b9786bf429cba3edbc1
Pedro368 <56154168+Pedro368@users.noreply.github.com>
2021-04-22 22:37:41 +0200

    Primer commit

---
+++ modulo02/program.py
@@ -0,0 +1,30 @@
+numero2 = 400
+numero4 = 500
+nombre = "55"
+print(float(nombre))
+print(numero2)
+print(float(nombre) + 25)
+print(float(nombre))
+
+#isdigit() no vacio y contiene solo numeros devuelve true o false
+#replace("o", "+")
+#Hola todos == H+la t+d+s
+#strip()
+#-    Hola a todos-
+#-Hola a todos-
+#Capitalize()
+#lower()
+#upper()
+nombre2 = "pepe2"
+nombre4= "321412"
+saludo3 = "Hola que tal2"
+print(nombre2.replace("e", "W"))
+print(nombre2.upper())
+print(nombre2.isdigit())
+print(nombre4.isdigit())
+
+variable = input("dime tu nombre: ")
+print("Dices que tu nombre es : " + variable)
+print(f"Dices que tu nombre es : {variable}")
+
+mensaje = "Mundo"
